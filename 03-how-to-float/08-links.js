3a07f3f5e603b6f86f1b11747240ff8df0959b17
irek <mail@gumen.pl>
2021-09-23 13:15:18 +0200

    update

---
+++ 03-how-to-float/08-links.js
@@ -0,0 +1,27 @@
+/* Useful links
+
+   MDN Number
+      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+
+   MDN Numeric literals
+      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#numeric_literals
+
+   MDN BigInt
+      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+
+   Floating Point Math 0.1 + 0.2 != 0.3
+      https://0.30000000000000004.com
+
+   Wikipedia IEEE 754
+      https://ew.wikipedia.org/wiki/IEEE_754
+
+   Wikipedia float64
+      https://en.wikipedia.org/wiki/Double-precision_floating-point_format
+
+   What Every Computer Scientist Should Know About Floating-Point Arithmetic
+      https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
+
+   IEEE-754 Floating Point Converter
+      https://www.h-schmidt.net/FloatConverter/IEEE754.html
+
+*/
