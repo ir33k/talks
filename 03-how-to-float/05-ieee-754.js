c2dd528545938aaeca43ba43d4bd572a96c5dc92
irek <mail@gumen.pl>
2021-09-23 09:43:21 +0200

    update

--- 03-how-to-float/05-ieee-754.js
+++ 03-how-to-float/05-ieee-754.js
@@ -1,17 +1,12 @@
 /* IEEE 754 https://ew.wikipedia.org/wiki/IEEE_754
 
-   JS uses double-precision floating-point format (FP64 or float64) that
-   is like 64 bit version of 32bit original IEEE 754 design.
+   1. Definition                                          (S - Sign bit)
 
-   https://en.wikipedia.org/wiki/Double-precision_floating-point_format
+      S Exponent Mantissa 23 (+1)
+      - -------- -----------------------
+      0 00000000 00000000000000000000000     Equation: S (1+M)*2^(E-127)
 
-   1. Definition                                          (S* - Sign bit)
-
-      S* Exponent  Mantissa
-      -  --------  -----------------------
-      0  00000000  00000000000000000000000    Equation: S (1+M)*2^(E-127)
-
-   2. Converting example  -23.3 => 1 10000011 01110100110011001100110
+   2. Example -23.3 => 1 10000011 01110100110011001100110
 
       a) 0.3 * 2 == 0.6  | 0    b) 10111.01001100110011001100110
          0.6 * 2 == 1.2  | 1    c) 1.011101001100110011001100110 * 2^4
@@ -23,11 +18,15 @@
       console.log((0.3).toString(2)) // => 0.01001100110011001100110
       console.log(( 23).toString(2)) // => 10111
       console.log((131).toString(2)) // => 10000011
+      
+/* 3. Example 1 10000011 01110100110011001100110 => -23.29999923706... */
 
-/* 3. Special values
+      console.log(0b10000011 - 127) // => 4
+      console.log(0b10111)          // => 23
 
-      0 00000000 00000000000000000000000 ==  0
-      1 00000000 00000000000000000000000 == -0
-      0 11111111 00000000000000000000000 ==  infinity ==  1/0
-      1 11111111 00000000000000000000000 == -infinity == -1/0
-      0 11111111 00101110101010010101111 ==  NaN      ==  0/0          */
+      /** Get decimal representation of fraction in given base */
+      const fun = (f, b=2) =>
+        Array.from(f).reduceRight((r,a) => (r+parseInt(a,b))/b,0)
+
+      console.log(fun('0100110011001100110')) // => 0.2999992370605469
+      console.log(-1*(23+0.2999992370605469)) // => -23.299999237060547
