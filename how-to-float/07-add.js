1af378d35cdf7e5d8478e5c0239618f0cb3453a1
irek <mail@gumen.pl>
2021-10-02 10:14:44 +0200

    Rename dirs

--- how-to-float/07-add.js
+++ how-to-float/07-add.js
@@ -22,9 +22,13 @@
       
    console.log((0.299999982119).toFixed(1))         // => 0.3
 
-   const buffer  = new ArrayBuffer(4)               // 32 bits
+   const buffer  = new ArrayBuffer(4*4)               // 32 bits
    const int32   = new Int32Array(buffer)
    const float32 = new Float32Array(buffer)
    
    int32[0] = 0b00111110100110011001100110011001
-   console.log(float32[0])                          // => 0.299999982119
+   int32[1] = 0b01111111100000000000000000000001
+   int32[2] = 0b01111111110000000000000000000001
+   int32[3] = 0b01111111100000000111111110000001
+
+   console.log(float32)                          // => 0.299999982119
